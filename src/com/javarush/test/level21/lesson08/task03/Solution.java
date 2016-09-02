38b9d672838ab54c18b9b0ecb25ac002f2ca8211
Igor <igorpekarskij.brest@gmail.com>
2016-09-03 01:02:37 +0300

    Create Solution.java

---
+++ src/com/javarush/test/level21/lesson08/task03/Solution.java
@@ -0,0 +1,55 @@
+/* Запретить клонирование
+Разрешите клонировать класс А
+Запретите клонировать класс B
+Разрешите клонировать класс C
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+        public Object clone() throws CloneNotSupportedException{
+            Object o = super.clone();
+            return o;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+        public Object clone()
+                throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+       public Object clone() throws CloneNotSupportedException{
+            
+            return new C(getI(),getJ(), getName());
+        }
+    }
+}
