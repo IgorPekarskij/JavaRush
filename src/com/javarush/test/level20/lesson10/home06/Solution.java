3e2ba7af9915ed22658cc8fe354dc13a80d43ea9
Igor <igorpekarskij.brest@gmail.com>
2016-09-04 23:34:31 +0300

    Create Solution.java

---
+++ src/com/javarush/test/level20/lesson10/home06/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+/* Запрет сериализации
+Запретите сериализацию класса SubSolution используя NotSerializableException.
+Сигнатуры классов менять нельзя
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            throw new NotSerializableException();
+        }
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            throw new NotSerializableException();
+        }
+    }
+}
